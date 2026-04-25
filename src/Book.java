d57549d3292b66ee0116e5ddbc6ed88e2af46711
devmc <esalpagarow@gmail.com>
2026-04-25 13:55:04 +0300

    Added a few new classes

---
+++ src/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    private String title;
+    private String author;
+    private int pages;
+
+    public Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    public String printInfo() {
+        return "Book: Harry Potter, Rowling, 500 pages";
+    }
+}
